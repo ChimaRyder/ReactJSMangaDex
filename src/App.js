62c1155bb50f7c24e4cecf96f3d20d13365f58a7
Frenz R <125328585+ChimaRyder@users.noreply.github.com>
2024-02-28 18:52:04 +0800

    (NOT WORKING) ReactJS practice on a MangaDexAPI with Boostrap Elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,128 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {Manga, resolveArray} from "mangadex-full-api";
+import {getValue} from "@testing-library/user-event/dist/utils";
+import React, {useState} from "react";
+
+
+function CardTemplate({key, coverimg, title, text, button_text}) {
+  return (
+      <Col>
+          <Card style={{ width: '18rem' }} key={key}>
+            <Card.Img variant="top" src={coverimg} />
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>
+                {text}
+              </Card.Text>
+              <Button variant="primary">{button_text}</Button>
+            </Card.Body>
+          </Card>
+      </Col>
+  );
+}
+
+let hasChanged = false;
+// function titleChanger(title) {
+//     DisplayManga(title);
+// }
+
+function DisplayManga() {
+    const [list, setList] = useState(null);
+    const [title, setTitle] = useState('');
+    hasChanged = true;
+
+
+
+    Manga.search({
+        title: title,
+        limit: 10,
+        hasAvailableChapters: true, }).then(mangas => {
+            setList(mangas)
+
+    }).catch(e=> console.log(e))
+
+    return (
+        <>
+        <Form>
+            <Form.Group className="mb-3" controlId="searchbar">
+                <Form.Label>Manga</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Search For Manga..."
+                    onKeyDown={e => {
+                        if (e.key === 'Enter') {
+                            e.preventDefault()
+                            setTitle(e.target.value)
+                        }
+
+                    }}
+                />
+            </Form.Group>
+        </Form>
+
+        <Container>
+            <Row>
+                { list != null ?
+                    list.map(({id, localTitle, description}) =>
+                        <CardTemplate
+                            key={id}
+                            title={localTitle}
+                            text={ description.en }
+                            button_text="Read"
+                        ></CardTemplate> )
+                    :
+                        <p>No Manga Here...</p>
+                }
+            </Row>
+        </Container>
+        </>
+    )
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+        {/*<Container>*/}
+        {/*    <Row>*/}
+        {/*        /!*<CardTemplate*!/*/}
+        {/*        /!*    coverimg= 'guidephoto_alegria.jpg'*!/*/}
+        {/*        /!*    title= "Bali"*!/*/}
+        {/*        /!*    text="A place to visit in Indonesia. Hopefully it's good."*!/*/}
+        {/*        /!*    button_text="More Info"></CardTemplate>*!/*/}
+        {/*        /!*<CardTemplate*!/*/}
+        {/*        /!*    coverimg= 'guidephoto_alegria.jpg'*!/*/}
+        {/*        /!*    title= "China"*!/*/}
+        {/*        /!*    text="Wonderful place in the east, with beautiful landscapes."*!/*/}
+        {/*        /!*    button_text="More Info"></CardTemplate>*!/*/}
+        {/*        /!*<CardTemplate*!/*/}
+        {/*        /!*    coverimg= 'guidephoto_alegria.jpg'*!/*/}
+        {/*        /!*    title= "Wakayama"*!/*/}
+        {/*        /!*    text="Japan's premiere waterfall destination"*!/*/}
+        {/*        /!*    button_text="More Info"></CardTemplate>*!/*/}
+
+        {/*        <listdisplay></listdisplay>*/}
+
+        {/*    </Row>*/}
+        {/*</Container>*/}
+
+        {/*{hasChanged === false ?*/}
+        {/*    <p>No manga here</p>*/}
+        {/*    :*/}
+            <DisplayManga></DisplayManga>
+        {/*}*/}
     </div>
   );
 }
